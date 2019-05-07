17bc8bceac31464f87149c3cfa6566cf41565ee6
hongluu <honglm1011@gmail.com>
2019-05-07 17:27:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import ACrawler from "./a_crawler"
+// import ACrawler from "./a_crawler"
 
-const fs = require("fs")
-const log4js = require('log4js');
+// const fs = require("fs")
+// const log4js = require('log4js');
 
-log4js.configure({
-    appenders: { web_crawler: { type: 'file', filename: 'web-crawler.log' } },
-    categories: { default: { appenders: ['web_crawler'], level: 'debug' } }
-});
+// log4js.configure({
+//     appenders: { web_crawler: { type: 'file', filename: 'web-crawler.log' } },
+//     categories: { default: { appenders: ['web_crawler'], level: 'debug' } }
+// });
 
-//init log
-let logger = log4js.getLogger("web_crawler");
-//init config
-let config = {
-    name: 'crawl-storage-1',
-    origin_url: 'https://en.wikipedia.org',
-    should_visit_prefix: ['https://en.wikipedia.org/'],
-    page_data_prefix: ['https://en.wikipedia.org/wiki/'],
-    max_depth:3
-};
+// //init log
+// let logger = log4js.getLogger("web_crawler");
+// //init config
+// let config = {
+//     name: 'crawl-storage-1',
+//     origin_url: 'https://en.wikipedia.org',
+//     should_visit_prefix: ['https://en.wikipedia.org/'],
+//     page_data_prefix: ['https://en.wikipedia.org/wiki/'],
+//     max_depth:3
+// };
 
-let crawler = new ACrawler(config, logger);
-crawler.start();
+// let crawler = new ACrawler(config, logger);
+// crawler.start();
+
+import Crawler from "./crawler"
+export default Crawler;
