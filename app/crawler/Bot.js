5cffc224a1b3544115b011aea06ef12a385f99cd
hongluu <honglm1011@gmail.com>
2019-05-30 00:42:09 +0700

    Add contrains finished

--- app/crawler/Bot.js
+++ app/crawler/Bot.js
@@ -50,6 +50,7 @@ export default class Bot {
         this.request = axios.create({ timeout: 30000 });
         this.dataTest = [];
         this.isTesting = false;
+        this.isFinished = false;
     }
 
     _init_config(config) {
@@ -120,7 +121,7 @@ export default class Bot {
         let queue_size = this.getQueueSizeBy(this.config.time_delay);
         await this.processPage(this.config.origin_url)
         while (true) {
-            if (this.assignUrls.length == 0) {
+            if (this.assignUrls.length == 0 || !this.isFinished ) {
                 return;
             }
             await this.limiter.schedule(() => Promise.all(this.getNextUrls(queue_size).map(url => {
